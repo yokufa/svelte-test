12b77a5381cd33902c0a7b0947d25302b624cc04
yokufa <fagamewer@yandex.ru>
2024-03-18 19:25:17 +0300

    routes

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,9 +2,11 @@
     <div class="nav-container" >
     <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
     <div class="nav-links">
-    {#each nav as link}
-    <a href={link.href} class="link">{link.title}</a>
-    {/each}
+        {#each nav as link}
+        {#if link.isShow}
+        <a href={link.href} class="link">{link.title}</a>
+        {/if}
+        {/each}
     </div>
     </div>
     </nav>
@@ -16,23 +18,28 @@
     const nav = [
         {
             title: 'Обо мне',
-            href: '/about'
+            href: '/about',
+            isShow: true,
         },
         {
             title: 'Блог',
-            href: '/blog'
+            href: '/blog',
+            isShow: true,
         },
         {
             title: 'Портфолио',
-            href: '/project'
+            href: '/project',
+            isShow: true,
         },
         {
             title: 'Контакты',
-            href: '/contact'
+            href: '/contact',
+            isShow: true,
         },
         {
             title: 'temp',
-            href: '/temp'
+            href: '/temp',
+            isShow: false,
         },
     ]
 </script>
---
+++ src/routes/blog/routes.js
@@ -0,0 +1,7 @@
+export const routes = [
+    {
+    id: 1,
+    title: 'My First Blog Post',
+    body: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Blanditiis dignissimos dolorem ea ex maiores minima minus nisi perspiciatis sapiente voluptate',
+    },
+    ];
