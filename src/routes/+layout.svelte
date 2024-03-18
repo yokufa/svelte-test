f88fe56e5e7a3c17a667878fac56f8bca4733925
yokufa <fagamewer@yandex.ru>
2024-03-18 18:34:09 +0300

    add pages

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,9 @@
     <div class="nav-container" >
     <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
     <div class="nav-links">
-    <a href="/about" class="link">Обо мне</a>
-    <a href="/blog" class="link">Блог</a>
-    <a href="/projects" class="link">Портфолио</a>
-    <a href="/contact" class="link">Контакты</a>
+    {#each nav as link}
+    <a href={link.href} class="link">{link.title}</a>
+    {/each}
     </div>
     </div>
     </nav>
@@ -13,7 +12,30 @@
     <!-- Pages will be injected below -->
     <slot></slot>
     </div>
-
+<script>
+    const nav = [
+        {
+            title: 'Обо мне',
+            href: '/about'
+        },
+        {
+            title: 'Блог',
+            href: '/blog'
+        },
+        {
+            title: 'Портфолио',
+            href: '/project'
+        },
+        {
+            title: 'Контакты',
+            href: '/contact'
+        },
+        {
+            title: 'temp',
+            href: '/temp'
+        },
+    ]
+</script>
     <style>
     .container {
     max-width: 1400px;
