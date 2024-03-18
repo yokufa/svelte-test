96cbd7feae11bb5112a17b9de2b66cbab9a6c2cf
yokufa <fagamewer@yandex.ru>
2024-03-18 18:12:57 +0300

    add main page and nav

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<nav>
+    <div class="nav-container" >
+    <a href="/" class="nav-logo" title="Back to Homepage">Имя Фамилия</a>
+    <div class="nav-links">
+    <a href="/about" class="link">Обо мне</a>
+    <a href="/blog" class="link">Блог</a>
+    <a href="/projects" class="link">Портфолио</a>
+    <a href="/contact" class="link">Контакты</a>
+    </div>
+    </div>
+    </nav>
+    <div class="container">
+    <!-- Pages will be injected below -->
+    <slot></slot>
+    </div>
+
+    <style>
+    .container {
+    max-width: 1400px;
+    margin: 50px auto;
+    }
+    </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,86 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+<title> Имя Фамилия</title>
+</svelte:head>
+
+<div class='container'>
+<div class='info'>
+<div class='animate-background'>
+<div class='container info-container'>
+<div class='info-text'>
+<h1>
+<p class='animate-text-1'>Lorem ipsum dolor sit,</p>
+<p class='text-2'>Lorem ipsum dolor sit amet.</p>
+</h1>
+</div>
+<h2 class='subtitle'>
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Et repudiandae corrupti quo quas, vel itaque, aliquid esse sit neque nihil fugit culpa totam, distinctio omnis provident. Distinctio iusto accusamus dignissimos?
+</h2>
+<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+<div style='color:black' class='scroll-button animate-scroll' tabindex='0'>
+<a href='/about'>Click Here</a>
+<div class='arrow'>→</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+<div class='background-div'></div>
+
+<style>
+    .info {
+        text-align: center;
+        position: relative;
+    }
+
+    .info-container {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        text-align: left;
+        align-items: start;
+    }
+
+    .container {
+        max-width: 1400px;
+        margin: 190px 0 0 2.5rem;
+    }
+
+    .info h1 {
+        font-size: 4rem;
+        margin: 0;
+    }
+
+    .info h1 p {
+        position: relative;
+        font: inherit;
+        margin: 0;
+        text-align: left;
+        line-height: 130%;
+        width: fit-content;
+        font-weight: 800;
+        letter-spacing: -0.03em;
+    }
+
+    .info h2 {
+        background-color: #ecebe8;
+        width: 100%;
+        height: 56vh;
+        -moz-image-region: -205px auto;
+    }
+
+    .info .info-text {
+        position: relative;
+    }
+
+    .scroll-button {
+        display: flex;
+        border: none;
+        outline: none;
+        cursor: pointer;
+    }
+
+    .scroll-button .arrow {
+        margin-left: 0.50rem;
+    }
+</style>
